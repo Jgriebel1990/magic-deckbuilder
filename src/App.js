d0201739b16b9a505d565a195991ea206a2c6c27
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-23 19:44:36 -0400

    created handleLogin method to allow a user to log in. created a new component called Library that i want to render when someone logs in

--- src/App.js
+++ src/App.js
@@ -1,70 +1,92 @@
-import React, { Component } from 'react';
-import fb from './firebase-app';
-import './App.css';
-
+import React, { Component } from "react";
+import fb from "./firebase-app";
+import "./App.css";
+//Components
+import Library from './Library';
 
 class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            email: '',
-            password: '',
-            uid: ''
-        }
-        this.handleSignup = this.handleSignup.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
-    }
-
-    
-    handleSignup(e){
-        e.preventDefault();
-        fb.auth()
-                .createUserWithEmailAndPassword(this.state.email, this.state.password)
-                .then(response => (this.setState({
-                    email: response.email,
-                    uid: response.uid
-                })))
-                .catch(function(error) {
-            // Handle Errors here.
-            console.log(error)
-            
-            // ...
-        });
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      uid: ""
     };
+    this.handleSignup = this.handleSignup.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-    handleLogin(e){
+  handleSignup(e) {
+    e.preventDefault();
+    fb.auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(response => {
         this.setState({
-            email: e.target.value
-        });
-    };
-    render(){
-        return(
-            <div>
-                <h1>Magic the Gathering Deckbuilder</h1>
-                <form onSubmit={this.handleSignup}>
-                    <h2>Signup</h2>
-                    <input type="email" placeholder='Email' value={this.state.email} onChange={e => this.setState({email: e.target.value})}/>
-                    <input type="password" placeholder='Password' value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
-                    <button >Signup</button>
-                </form>
-                <form action="">
-                    <h2>Login</h2>
-                    <input type="email" placeholder='Email'/>
-                    <input type="password" placeholder='Password'/>
-                    <button>Login</button>
-                </form>
-                <div>
-                    <h1>Search</h1>
-                    <form action="">
-                        <input type="text"/>
-                    </form>
-                    <div className='card'>
+          email: response.email,
+          uid: response.uid
+        })
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  handleLogin(e) {
+    e.preventDefault();
+    fb.auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(response => {
+        this.setState({
+          email: response.email,
+          uid: response.uid
+        });
+      })
+      .catch(function(error) {});
+  }
+  render() {
+    if(fb.then === true){
+      return <Library />
+    }else {
+      null
+    };
+    return (
+      <div>
+        <h1>Magic the Gathering Deckbuilder</h1>
+        <form onSubmit={this.handleLogin}>
+          <h2>Signup</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <button>Signup</button>
+        </form>
+        <form onSubmit={this.handleLogin}>
+          <h2>Sign in</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <button>Sign in</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Library.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Library extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Search</h1>
+        <form>
+          <input type="text" />
+        </form>
+        <div className="card" />
+      </div>
+    );
+  }
+}
+
+
+export default Library;
