d263be974163337f704349d80aed5fed964f3ec4
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-22 21:11:04 -0400

    installed and created firebase module and put it into the handleSignup event

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import fb from './firebase-app';
 import './App.css';
+
 
 class App extends Component {
     constructor(){
@@ -11,6 +13,27 @@ class App extends Component {
         this.handleSignup = this.handleSignup.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
     }
+
+    
+    handleSignup(e){
+        firebase.auth()
+                .createUserWithEmailAndPassword(this.state.email, this.state.password)
+                .then(response => (this.setState({
+                    
+                })))
+                .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        });
+    };
+
+    handleLogin(e){
+        this.setState({
+            email: e.target.value
+        });
+    };
     render(){
         return(
             <div>
@@ -19,7 +42,7 @@ class App extends Component {
                     <h2>Signup</h2>
                     <input type="email" placeholder='Email'/>
                     <input type="password" placeholder='Password'/>
-                    <button onClick={}>Signup</button>
+                    <button >Signup</button>
                 </form>
                 <form action="">
                     <h2>Login</h2>
@@ -27,6 +50,15 @@ class App extends Component {
                     <input type="password" placeholder='Password'/>
                     <button>Login</button>
                 </form>
+                <div>
+                    <h1>Search</h1>
+                    <form action="">
+                        <input type="text"/>
+                    </form>
+                    <div className='card'>
+
+                    </div>
+                </div>
             </div>
         );
     }
