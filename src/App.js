947ce870696f6bea50c2c6d99144d2f1ad159eeb
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-22 20:25:29 -0400

    created state of email and password and binded them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.handleSignup = this.handleSignup.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
     render(){
         return(
             <div>
@@ -10,7 +19,7 @@ class App extends Component {
                     <h2>Signup</h2>
                     <input type="email" placeholder='Email'/>
                     <input type="password" placeholder='Password'/>
-                    <button>Signup</button>
+                    <button onClick={}>Signup</button>
                 </form>
                 <form action="">
                     <h2>Login</h2>
