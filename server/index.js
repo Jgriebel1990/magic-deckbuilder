6feebf4fd2b20a3da0becc22a9cb685246271fd2
jefferson Griebel <jefferson.griebel@gmail.com>
2018-03-26 16:25:50 -0400

    tried to set up my api but i am having trouble getting it to work

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
 //configuration
-require('dotenv').config();
-const { API_KEY } = process.env
+require("dotenv").config();
+const { API_KEY } = process.env;
 
 //creation
 const serverApp = express();
 const port = process.env.PORT || 5000;
+
+serverApp.use(express.static("client/build"));
+
+serverApp.get("/cards/:name,:multiverseid", function(request, response) {
+  const { name, multiverseid } = request.params;
+  const url = `https://api.magicthegathering.io/cards/${id},${name},${multiverseid}`;
+  axios
+    .get(url)
+    .then(res => {
+      response.status(200).json(res.data);
+    })
+    .catch(err => {
+      response.status(500).json({
+        msg: "error"
+      });
+    });
+});
+
+serverApp.get("*", (request, response) => {
+  response.sendFile("index.html", { root: path.resolve("client/build") });
+});
+
+serverApp.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
